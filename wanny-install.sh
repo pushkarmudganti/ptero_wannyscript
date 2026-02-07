d2dc4f19df6e64a4c8ffa93d5af273618fdce7cf
WannyDragon <pushkarmudganti@gmail.com>
2026-02-07 21:40:30 +0530

    Create wanny-install.sh

---
+++ wanny-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# WANNY Pterodactyl Installer - One-liner version
+echo "🚀 Installing WANNY Pterodactyl Installer..."
+
+# Download and run the full installer
+bash <(curl -s https://raw.githubusercontent.com/YourUsername/wanny-pterodactyl-installer/main/installer.sh)
